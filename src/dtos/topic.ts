76208e8923d00907eebf7d6c738637446037c06d
Koodji <guidatony15@gmail.com>
2017-10-06 13:12:05 +0200

    add mock + dtos

---
+++ src/dtos/topic.ts
@@ -0,0 +1,12 @@
+export class Topic {
+    id : number;
+    summary : string;
+    title : string;
+
+    constructor (id : number,summary:string,title : string){
+        this.id=id;
+        this.summary=summary;
+        this.title=title;
+    }
+
+}
