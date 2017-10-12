e4d2b14467d7915545f57dc140f0661f10f39ce0
Koodji <guidatony15@gmail.com>
2017-10-12 11:17:30 +0200

    rename service topic and add method to persit topic

--- src/services/topicService.ts
+++ src/services/topicService.ts
@@ -1,10 +1,14 @@
 import {Topic} from "../dtos/topic"
 
-export class DetailTopicService {
+export class topicService {
 
     retrieveTopicById( id : number){
         //retrieve topic with DAO
         return new Topic(1,"summary","title");
 
+    }
+
+    persiteTopic(topic:Topic){
+        //call DAO to persist Topic
     }
 }
