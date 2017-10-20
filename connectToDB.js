7e3cf5e68fe81530b7b5839dee15e5cb770d0a00
Koodji <guidatony15@gmail.com>
2017-10-20 14:39:42 +0200

    db final

--- connectToDB.js
+++ connectToDB.js
@@ -39,8 +39,19 @@ function handle_database(req,res) {
 }
 
 app.get("/",function(req,res){-
+    res.setHeader('Access-Control-Allow-Origin', '*');
     
-    handle_database(req,res);
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        handle_database(req,res);
 });
+
 
 app.listen(3000);
