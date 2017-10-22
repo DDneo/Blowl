046a52844621963a663a4592aae6c8160eeae2c2
TGuillaume <Tristan.guillaume-ISC_France@fr.ibm.com>
2017-10-22 17:26:18 +0200

    [DB] modify schema

--- connectToDB.js
+++ connectToDB.js
@@ -8,7 +8,7 @@ var pool      =    mysql.createPool({
     user     : 'root',
     password : '',
     database : 'mydb',
-    debug    :  false
+    debug    :  true
 });
 
 var query = new Object(); // or var map = {};
@@ -16,40 +16,39 @@ query[0] = "select * from t_user where email=";
 
 
 function handle_database(req,res) {
-   
     pool.getConnection(function(err,connection){
       console.log(req.query["query"]);
         if (err) {
           res.json({"code" : 100, "status" : "Error in connection database"});
           return;
-        }  
+        }
 
         console.log('connected as id ' + connection.threadId);
-       
+
         connection.query(req.query["query"],function(err,rows){
             connection.release();
             if(!err) {
                 console.log("return data "+rows);
                 res.json(rows);
-            }          
+            }
         });
 
-        connection.on('error', function(err) {      
+        connection.on('error', function(err) {
               res.json({"code" : 100, "status" : "Error in connection database"});
-              return;    
+              return;
         });
   });
 }
 
 app.get("/",function(req,res){-
     res.setHeader('Access-Control-Allow-Origin', '*');
-    
+
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
+
         // Request headers you wish to allow
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    
+
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
         res.setHeader('Access-Control-Allow-Credentials', true);
